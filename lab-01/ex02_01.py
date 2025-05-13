35dcd0f4997bcf12e65de9d2ea261147734261ba
vuthanhdat37 <vuthanhdat37.37@gmail.com>
2025-05-13 20:28:23 +0700

    Xóa file hello.py khỏi GitHub

--- lab-01/ex02_01.py
+++
@@ -1,5 +0,0 @@
-#nhap ten va tuoi tu nguoi dung
-ten = input("nhap ten cua ban: ")
-tuoi = input ("nhap tuoi cua ban: ")
-#in thong diep chao mung thong tin vua nhap
-print ("Chào mừng ,", ten, "! Bạn", tuoi, "tuổi.")
